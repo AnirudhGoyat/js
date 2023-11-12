f5702806241ff1ce40ea5569a4845ac5259d8616
Anirudh Goyat <76870336+AnirudhGoyat@users.noreply.github.com>
2023-11-12 19:35:11 +0000

    data  type

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,20 @@
 const accountId =123455
 let accountEmail = "anirudhgoet@gmail.com"
 var accountPassword = "12345"
-
 accountCity = "Gurugram"
+
+let accountState
 
 // accountId= 2 //not allowed 
 accountEmail  = "ani@gmail.com"
 accountPassword = "212121"
 accountCity = "delhi" 
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using NodeJs not browser so alert will give error
+
+console.log( 3 
+    +3 ); // code readability should be high  
+
+console.log("anirudh");    
+
+
+let name = "anirudh"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt (for very large number)
+// string => ""
+// boolean => true/false
+// null => standalone value // null is an object itself
+// undefined => 
+// symbol => uniqueness  
+
+//object 
+
+console.log(typeof undefined); // type => undefined 
+console.log(typeof null); // type=> object
