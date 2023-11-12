2ee1a1064bda7ef21e448464f78675c2b9bb0cbb
Anirudh Goyat <76870336+AnirudhGoyat@users.noreply.github.com>
2023-11-12 09:16:45 +0000

    variable changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =123455
+let accountEmail = "anirudhgoet@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Gurugram"
+
+// accountId= 2 //not allowed 
+accountEmail  = "ani@gmail.com"
+accountPassword = "212121"
+accountCity = "delhi" 
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
