a24332cc961a0f211064d17bc0c9d9ac5839c318
Anirudh Goyat <76870336+AnirudhGoyat@users.noreply.github.com>
2023-11-13 18:03:40 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,5 +32,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************** Operations **********************
+
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2  = " anirudh"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +  2 + 2 ); // string is 1st to taken as string 
+// console.log( 1 +  2 + "2" ); // 1st number so value calculated and then acted as string
+ 
+// console.log( true + false);
+// console.log(+"")
+
+let num1, num2, num3
+
+// num1 = num2 = num2 = 2+2  // not readable 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
