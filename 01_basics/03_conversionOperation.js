17204446257eee38a18f605c8f781f8c193f83b3
Anirudh Goyat <76870336+AnirudhGoyat@users.noreply.github.com>
2023-11-13 12:15:32 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+let valueInNumber = Number(score) // Number , String etc can be used this way to convert the datatypes from one form to another form
+
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber); // NaN is a special type
+
+
+
+//  "33" => 33
+//  "33abc" => NaN (Not a number)
+//  null => 0
+//  undefined => NaN
+//  true => 1 ; false => 0
+
+let isLoggedIn = "anirudh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+
+//  1 => true ; 0 => false  // number conversion can also be done to boolean
+//  "" => false
+//  "anirudh" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
